708d7d34b35e05e57d932a553354144b8ec412c1
Nathan Downes <you@example.com>
2021-03-02 21:38:17 +0000

    update network proxy

--- src/NetworkRequest/fetch.js
+++ src/NetworkRequest/fetch.js
@@ -1,12 +1,11 @@
 const fetchQuotes = () =>
   fetch(
-    "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
-  ).then(response => response.json());
-
+    "https://api.codetabs.com/v1/proxy?quest=http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+  ).then((response) => response.json());
 
 const returnQuote = async () => {
   let { quoteText, quoteAuthor } = await fetchQuotes();
-    return { quoteText, quoteAuthor };
+  return { quoteText, quoteAuthor };
 };
 
 export default returnQuote;
